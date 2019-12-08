055ed3056f2a41b5485af777f29507ec40a4f0dd
peterwmwong <peter.wm.wong@gmail.com>
2019-12-08 11:38:03 -0600

    d2: part2

--- d2/src/main.rs
+++ d2/src/main.rs
@@ -1,25 +1,27 @@
 use std::ops::{Add, Mul};
 
-fn run_op<F: FnOnce(usize, usize) -> usize>(pc: usize, prog: &mut Vec<usize>, f: F) {
+type Prog = Vec<usize>;
+
+fn read_prog(path: &str) -> Prog {
+    std::fs::read_to_string(path)
+        .unwrap()
+        .trim()
+        .split(',')
+        .map(|s| s.parse::<usize>().unwrap())
+        .collect()
+}
+
+fn run_op<F: FnOnce(usize, usize) -> usize>(pc: usize, prog: &mut Prog, f: F) {
     let a = prog[pc + 1];
     let b = prog[pc + 2];
     let o = prog[pc + 3];
     prog[o] = f(prog[a], prog[b]);
 }
 
-fn main() {
-    let mut prog: Vec<usize> = std::fs::read_to_string("./input.txt")
-        .unwrap()
-        .trim()
-        .split(',')
-        .map(|s| s.parse::<usize>().unwrap())
-        .collect();
-
-    // "... replace position 1 with the value 12 and replace position 2 with the value 2"
-    prog[1] = 12;
-    prog[2] = 2;
-
-    for pc in (0..prog.len()).step_by(4) {
+fn run_prog(mut prog: Prog, noun: usize, verb: usize) -> usize {
+    prog[1] = noun;
+    prog[2] = verb;
+    for pc in (0..).step_by(4) {
         match prog[pc] {
             1 => run_op(pc, &mut prog, Add::add),
             2 => run_op(pc, &mut prog, Mul::mul),
@@ -27,5 +29,29 @@ fn main() {
             _ => panic!("Unknown opcode"),
         }
     }
-    println!("{:?}", prog);
+    prog[0]
+}
+
+fn part1(prog: &Prog) {
+    // "... replace position 1 with the value 12 and replace position 2 with the value 2"
+    println!("part1 - answer {}", run_prog(prog.clone(), 12, 2));
+}
+
+fn part2(prog: &Prog) {
+    for noun in 0..=99 {
+        for verb in 0..=99 {
+            if run_prog(prog.clone(), noun, verb) == 19690720 {
+                let answer = 100 * noun + verb;
+                println!("part2 - noun {} verb {} answer {}", noun, verb, answer);
+                return;
+            }
+        }
+    }
+    panic!("No solution found");
+}
+
+fn main() {
+    let prog: Prog = read_prog("./input.txt");
+    part1(&prog);
+    part2(&prog);
 }
